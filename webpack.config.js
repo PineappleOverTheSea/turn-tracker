e70eec9e5707bdee8ebaffb0cafc12ee8d915fe9
Kramst <hhalooven@gmail.com>
2021-10-25 21:27:31 +0300

    more loaders setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production")
@@ -9,15 +11,43 @@ module.exports = {
         static: ["dist"],
         hot: true
     },
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]",
+        clean: true
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.s?css$/i,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: ""}
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "scss-loader"
+                ],
+
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset"
             }
         ]
-    }
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    plugins: [
+        new MiniCssExtractPlugin
+    ]
 }
