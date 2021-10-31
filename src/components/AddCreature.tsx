5cb1c225728c3dd4b8ae6e945d5ecf7e9c159e92
Kramst <hhalooven@gmail.com>
2021-10-31 20:14:05 +0200

    Sukurtas padaro pridėjimo išdėstymas

--- src/components/AddCreature.tsx
+++ src/components/AddCreature.tsx
@@ -47,66 +47,71 @@ const AddCreatureModal = (props: any) => {
     
     const onSetHpMaximum = (e : React.ChangeEvent<HTMLInputElement>) => {
         const health = parseInt(e.target.value)
-        console.log(health);
         setHpMax(health);
         setHp(health);
-        console.log(hp);
     }
 
     return (
         <div className="add-creature">
-            <h3>Pridėti padarą</h3>
-            <div className="stat">
-                <label htmlFor="">Name</label>
-                <input type="text" name="creature-name" id="creature-name" value={name} onChange={e => setName(e.target.value)} />
+            <div className="add-creature-info">
+                <div className="stat">
+                    <input type="text" name="creature-name" id="creature-name" value={name} onChange={e => setName(e.target.value)} />
+                </div>
             </div>
-            <div className="stat">
-                <label htmlFor="">Hit Point Maximum</label>
-                <input type="number" name="hit-point-maximum" id="hp-max" value={hpMax} onChange={e => onSetHpMaximum(e)} />
+            <div className="add-creature-health">
+                <div className="stat">
+                    <label htmlFor="">Maximum HP</label>
+                    <input type="number" name="hit-point-maximum" id="hp-max" value={hpMax} onChange={e => onSetHpMaximum(e)} />
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Current HP</label>
+                    <input type="number" name="hit-points" id="hp" value={hp} onChange={e => setHpTemp(parseInt(e.target.value))} />
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Temporary HP</label>
+                    <input type="number" name="hit-points-temporary" id="hp-temp" value={hpTemp} onChange={e => setHp(parseInt(e.target.value))} />
+                </div>
             </div>
-            <div className="stat">
-                <label htmlFor="">Hit Points</label>
-                <input type="number" name="hit-points" id="hp" value={hp} onChange={e => setHpTemp(parseInt(e.target.value))} />
+            <div className="add-creature-cmbt-stats">
+                <div className="stat">
+                    <label htmlFor="">Initiative</label>
+                    <input type="number" name="initative" id="init" value={init} onChange={e => setInit(parseInt(e.target.value))} />
+                    <div>+ {Math.floor((dex-10)/2)} = {Math.floor((dex-10)/2 + init)}</div> 
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Armor Class</label>
+                    <input type="number" name="armor-class" id="ac" value={ac} onChange={e => setAc(parseInt(e.target.value))}/>
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Speed</label>
+                    <input type="number" name="speed" id="spd" value={spd} onChange={e => setSpd(parseInt(e.target.value))}/>
+                </div>
             </div>
-            <div className="stat">
-                <label htmlFor="">Temporary Hit Points</label>
-                <input type="number" name="hit-points-temporary" id="hp-temp" value={hpTemp} onChange={e => setHp(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Strength</label>
-                <input type="number" name="strength" id="str" value={str} onChange={e => setStr(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Dexterity</label>
-                <input type="number" name="dexterity" id="dex" value={dex} onChange={e => setDex(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Constitution</label>
-                <input type="number" name="constitution" id="con" value={con} onChange={e => setCon(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Inteligence</label>
-                <input type="number" name="inteligence" id="int" value={int} onChange={e => setInt(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Wisdom</label>
-                <input type="number" name="wisdom" id="wis" value={wis} onChange={e => setWis(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Charisma</label>
-                <input type="number" name="charisma" id="chr" value={cha} onChange={e => setCha(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Initiative</label>
-                <input type="number" name="initative" id="init" value={init} onChange={e => setInit(parseInt(e.target.value))} />
-            </div>
-            <div className="stat">
-                <label htmlFor="">Armor Class</label>
-                <input type="number" name="armor-class" id="ac" value={ac} onChange={e => setAc(parseInt(e.target.value))}/>
-            </div>
-            <div className="stat">
-                <label htmlFor="">Speed</label>
-                <input type="number" name="speed" id="spd" value={spd} onChange={e => setSpd(parseInt(e.target.value))}/>
+            <div className="add-creature-stats">
+                <div className="stat">
+                    <label htmlFor="">Strength</label>
+                    <input type="number" name="strength" id="str" value={str} onChange={e => setStr(parseInt(e.target.value))} />
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Dexterity</label>
+                    <input type="number" name="dexterity" id="dex" value={dex} onChange={e => setDex(parseInt(e.target.value))} />
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Constitution</label>
+                    <input type="number" name="constitution" id="con" value={con} onChange={e => setCon(parseInt(e.target.value))} />
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Inteligence</label>
+                    <input type="number" name="inteligence" id="int" value={int} onChange={e => setInt(parseInt(e.target.value))} />
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Wisdom</label>
+                    <input type="number" name="wisdom" id="wis" value={wis} onChange={e => setWis(parseInt(e.target.value))} />
+                </div>
+                <div className="stat">
+                    <label htmlFor="">Charisma</label>
+                    <input type="number" name="charisma" id="chr" value={cha} onChange={e => setCha(parseInt(e.target.value))} />
+                </div>
             </div>
             <button onClick={addCreature}>Add Creature</button>
         </div>
