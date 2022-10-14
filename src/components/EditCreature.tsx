3e29cf4360d9a531d6b1a6a20c461f0e448897fb
PineappleOverTheSea <hhalooven@gmail.com>
2022-10-14 23:04:07 +0300

    fix

--- src/components/EditCreature.tsx
+++ src/components/EditCreature.tsx
@@ -5,25 +5,33 @@ import Creature from "./Creature/Creature";
 
 const EditCreature = () =>{
     const {trackedElements} = useContext(TrackedElementsContext);
-    let creatureToDisplay
+    let elementToDisplay
 
-    const selectedCreature = trackedElements.find(creature => (creature as ICreature).classList?.includes("selected"))
+    const selectedCreature = trackedElements.find(elem => elem.classList.includes("selected"))
 
     if(selectedCreature)
-        creatureToDisplay = trackedElements[trackedElements.indexOf(selectedCreature)];
+        elementToDisplay = trackedElements[trackedElements.indexOf(selectedCreature)] as ICreature;
 
     return(
         <div className="edit-creature">
             <div className="title">Edit existing creature:</div>                   
-            {creatureToDisplay ? <Creature 
-                key={(creatureToDisplay as ICreature).id}
-                id={(creatureToDisplay as ICreature).id}
-                classList={(creatureToDisplay as ICreature).classList}
-                name={(creatureToDisplay as ICreature).name}
-                
-                
-                
-            /> : <Creature id={-1} placeholder={true} /> }
+            {elementToDisplay ? <Creature 
+                key={elementToDisplay.id}
+                id={elementToDisplay.id}
+                classList={elementToDisplay.classList}
+                name={elementToDisplay.name}
+                strength={elementToDisplay.strength}
+                dexterity={elementToDisplay.dexterity}
+                constitution={elementToDisplay.constitution}
+                inteligence={elementToDisplay.inteligence}
+                wisdom={elementToDisplay.wisdom}
+                charisma={elementToDisplay.charisma}
+                hitPoints={elementToDisplay.hitPoints}
+                hitPointsMax={elementToDisplay.hitPointsMax}
+                hitPointsTemp={elementToDisplay.hitPointsTemp}
+                initiative={elementToDisplay.initiative}
+                armorClass={elementToDisplay.armorClass}
+                speed={elementToDisplay.speed}/> : <Creature id={-1} placeholder={true} /> }
         </div>
     )
 }
